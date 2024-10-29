693890eadb631f85161a00ca2265aee336ab91b6
LucasMMcode <144387466+LucasMMcode@users.noreply.github.com>
2024-10-29 12:26:34 -0300

    b
    b

--- AulasNode/projeto/src/index.js
+++ AulasNode/projeto/src/index.js
@@ -1,1 +1,19 @@
-console.log("Teste novo")
+let a = 10;
+let b = 3;
+console.log(a+b);//13
+console.log(a-b);//7
+console.log(a*b);//30
+console.log(a/b);//3.33333...
+console.log(a%b);//
+console.log(a**b);//1000
+
+let age = 18;
+if(age<18){
+    console.log('Você é menor de idade.');
+}
+else if(age===18){
+        console.log('Você acabo de se tornar maior de idade!');
+}
+else{
+    console.log('Você é maior de idade');
+}
