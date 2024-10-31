5e389d6637bb557d4527d9d7bee49b41a052b1dd
LucasMMcode <144387466+LucasMMcode@users.noreply.github.com>
2024-10-31 09:41:44 -0300

    a
    a

---
+++ AulasNode/exemploserver/exemploswitch.js
@@ -0,0 +1,14 @@
+let day =3;
+switch (day){
+    case 1:
+    console.log('Domingo');
+    break;
+    case 2:
+    console.log('Segunda-feira');
+    break;
+    case 3:
+    console.log('Terça-feira');
+    break;
+    default:
+        console.log('Dia inválido');
+}
